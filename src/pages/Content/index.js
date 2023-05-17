d33f9976f408e95c6a2e6a41ab5a3694092319ef
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-05-16 21:35:57 -0700

    implemented agent status bar

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,7 +1,8 @@
 import { printLine } from "./modules/print";
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import AgentStatusContainer from "./components/AgentStatus/AgentStatusContainer"
+import AgentStatusContainer from "./components/AgentStatusContainer"
+import './content.styles.css'
 
 console.log("Content script works!");
 console.log("Must reload extension for modifications to take effect.");
@@ -14,7 +15,7 @@ printLine("Using the 'printLine' function from the Print Module");
 
 const body = document.querySelector('body')
 const app = document.createElement('div')
-app.style.cssText = 'z-index:10;position:absolute;bottom:16px;width:100%'
+app.style.cssText = 'z-index:10;position:fixed;bottom:16px;width:100%;display:flex;justify-content:center;'
 
 app.id = 'react-root'
 
