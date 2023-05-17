df2e6d93c5fef53943c9006990c14d8c6fb62ef1
Janak <janak2@users.noreply.github.com>
2023-05-17 13:16:42 -0700

    added scraper to content

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,4 +1,6 @@
 import { printLine } from "./modules/print";
+import $ from "jquery";
+import { scrapeDOM } from "./modules/scraper";
 
 console.log("Content script works!");
 console.log("Must reload extension for modifications to take effect.");
@@ -6,5 +8,10 @@ console.log("Must reload extension for modifications to take effect.");
 chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
   console.log(response.farewell);
 });
+
+$(document).ready(() => {
+    scrapeDOM();
+});
+
 
 printLine("Using the 'printLine' function from the Print Module");
