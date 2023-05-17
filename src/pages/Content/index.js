365727f98b820646296523bc316cc795a4331d0f
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-05-17 16:07:02 -0700

    fix: refactored all agent status bar component in shadow-root

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,8 +1,9 @@
 import { printLine } from "./modules/print";
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import AgentStatusContainer from "./components/AgentStatus/AgentStatusContainer"
-import './content.styles.css'
+import React from "react";
+import AgentStatusContainer from "./components/AgentStatus/AgentStatusContainer";
+import "./content.styles.css";
+import { render } from "react-dom";
+import { StyleSheetManager } from "styled-components";
 
 console.log("Content script works!");
 console.log("Must reload extension for modifications to take effect.");
@@ -13,18 +14,36 @@ chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
 
 printLine("Using the 'printLine' function from the Print Module");
 
-const body = document.querySelector('body')
-const app = document.createElement('div')
-app.style.cssText = 'z-index:10000;position:fixed;bottom:16px;width:100%;display:flex;justify-content:center;'
+const body = document.querySelector("body");
+const app = document.createElement("div");
+app.style.cssText =
+  "z-index:10000;position:fixed;bottom:16px;width:100%;display:flex;justify-content:center;";
 
-app.id = 'react-root'
+app.id = "react-root";
 
 if (body) {
-  body.prepend(app)
+  body.prepend(app);
 }
 
-const container = document.getElementById('react-root');
-const root = createRoot(container);
-root.render(
+// const container = document.getElementById("react-root");
+// const root = createRoot(container);
+
+const host = document.querySelector("#react-root");
+const shadow = host.attachShadow({ mode: "open" });
+
+// create a slot where we will attach the StyleSheetManager
+const styleSlot = document.createElement("section");
+// append the styleSlot inside the shadow
+shadow.appendChild(styleSlot);
+
+// create the element where we would render our app
+const renderIn = document.createElement("div");
+// append the renderIn element inside the styleSlot
+styleSlot.appendChild(renderIn);
+
+render(
+  <StyleSheetManager target={styleSlot}>
     <AgentStatusContainer />
+  </StyleSheetManager>,
+  renderIn
 );
