c7e4f68dc5106edc8902d9ca75de48fe29cb10a0
Eugene Chan <15060312+eugenechantk@users.noreply.github.com>
2023-05-17 10:15:44 -0700

    fixed state styling + always show status bar

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,7 +1,7 @@
 import { printLine } from "./modules/print";
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import AgentStatusContainer from "./components/AgentStatusContainer"
+import AgentStatusContainer from "./components/AgentStatus/AgentStatusContainer"
 import './content.styles.css'
 
 console.log("Content script works!");
@@ -15,7 +15,7 @@ printLine("Using the 'printLine' function from the Print Module");
 
 const body = document.querySelector('body')
 const app = document.createElement('div')
-app.style.cssText = 'z-index:10;position:fixed;bottom:16px;width:100%;display:flex;justify-content:center;'
+app.style.cssText = 'z-index:10000;position:fixed;bottom:16px;width:100%;display:flex;justify-content:center;'
 
 app.id = 'react-root'
 
