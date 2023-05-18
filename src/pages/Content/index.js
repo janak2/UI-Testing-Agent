16af8077b1f68766a106592edb7d9806fc94beab
Selali Adobor <selali.adobor@gmail.com>
2023-05-17 21:07:11 -0700

    Add support for fetching quips

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -11,12 +11,21 @@ import { scrapeDOM } from "./modules/scraper";
 console.log("Content script works!");
 console.log("Must reload extension for modifications to take effect.");
 
-chrome.runtime.sendMessage({ greeting: "hello" }, function (response) {
-  console.log(response.farewell);
-});
+function requestFeedback(persona, domSummary) {
+  console.log("sending request")
+  chrome.runtime.sendMessage({
+      type: 'getFeedback',
+      persona: persona,
+      domSummary: domSummary
+  }, response => {
+      // handle the response here
+      console.log(response);
+  });
+}
 
 $(document).ready(() => {
-  scrapeDOM();
+  console.log("starting scrape")
+  requestFeedback("angry steve jobs", scrapeDOM())
 });
 
 printLine("Using the 'printLine' function from the Print Module");
